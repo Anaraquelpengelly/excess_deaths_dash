bbf74e78c1476636cefaccc8adb2ea250d95da02
Ana Pengelly <anaraquelpengelly@gmail.com>
2021-01-07 13:01:27 -0600

    put the dash together

--- per_lad/map_only_app copy.py
+++ per_lad/map_only_app copy.py
@@ -1,5 +1,3 @@
-
-
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
@@ -56,42 +54,13 @@ with open(ASSET_PATH.joinpath('Local_Authority_Districts_(December_2017)_General
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 
-
 #define layout
-app.layout = html.Div(
-    children=[
-        html.Div(className='row',
-                 children=[
-                    html.Div(className='two columns div-user-controls',
+app.layout = html.Div(className='ten columns div-for-charts bg-grey',
                              children=[
-                                 html.H2('Excess deaths per Local Authority in England and Wales in 2020'),
-                                 html.P('Visualising Excess deaths in the UK in 2020 at the local authority level. Data was downloaded from the ONS website.')
-                                 
-                                ]
-                             ),
-                    html.Div(className='ten columns div-for-charts bg-grey',
-                             children=[
-                                 html.H1(f'Excess deaths in {lads} in 2020', id="graph-title"),
-                                 html.H5('Pick a Local Authority or country from the dropdown below'),
-                                 html.Div(
-                                     className='div-for-dropdown',
-                                     children=[
-                                         dcc.Dropdown(id='ladselector', options=[{'label': i, 'value': i} for i in lads],
-                                        #  placeholder="Select a local authority",
-                                         value='England',
-                                         style={'backgroundColor': '#1E1E1E'},
-                                         className='ladselector'
-                                                      )
-                                     ],
-                                     
-                                     style={'color': '#1E1E1E',
-                                     'font-size': '18px'}),
-                                     html.H3(f'The total number of excess deaths in 2020 in {lads} .', id='total-num'),
-                                        dcc.Graph(id='timeseries', config={'displayModeBar': False}, animate=False),
-                                     html.H1(f'Excess deaths in 2020', id='title_weeks'),
+                                     html.H1(f'Excess deaths in 2020'),
                                      html.Div(id='radio-item-container', 
                                      children=[
-                                         html.H5("Choose a time period: "),
+                                         html.H3("Choose a time period: "),
                                          dcc.RadioItems(
                                             id='period', 
                                             options=[{'value': x, 'label': x} 
@@ -102,7 +71,7 @@ app.layout = html.Div(
                                      html.Div(
                                         id="slider-container",
                                         children=[
-                                                html.H5(
+                                                html.H3(
                                                 id="slider-text",
                                                 children="Drag the slider to change the week:"),
                                                 dcc.Slider(
@@ -115,76 +84,13 @@ app.layout = html.Div(
                                                         "label": str(w),
                                                         "style": {"color": "white", 'font-family':'Roboto'},} for w in weeks})
                                                         ]),
-                                                        html.H3(f'Excess deaths in week 2020.', id='weekly-excess-deaths'),
+                                                        html.H2(f'Excess deaths in week 2020.', id='title_weeks'),
                                                         dcc.Graph(id='map', config={'displayModeBar': False}, animate=False
                                                         )
                              ])
-                              ])
-        ]
-
-)
 
 
-# Callback for timeseries deaths
-@app.callback(Output('timeseries', 'figure'),
-              [Input('ladselector', 'value')])
-def update_graph(selected_dropdown_value):
-    trace1 = []
-    df_sub = df[df['Area_Name'] == selected_dropdown_value]
-    trace1.append(go.Scatter(x=df_sub['Week_Number'],
-                                 y=df_sub['reg_deaths_20'],
-                                 mode='lines',
-                                 marker_color = 'magenta',
-                                 opacity=0.7,
-                                 name="Deaths in 2020",
-                                 textposition='bottom center'))
-    trace2= []
-    df_sub = df[df['Area_Name'] == selected_dropdown_value]
-    trace2.append(go.Scatter(x=df_sub['Week_Number'],
-                                 y=df_sub['avg_reg_deaths_15_19'],
-                                 mode='lines',
-                                 marker_color='mintcream',
-                                 opacity=0.7,
-                                 name="Average deaths from 2015 to 2019",
-                                 textposition='bottom center'))
-    
-    traces = [trace1, trace2]
-    data = [val for sublist in traces for val in sublist]
-    layout = go.Layout(
-        #title={'text': f"Excess deaths in "+selected_dropdown_value+ " in 2020", 'font': {'color': 'white'}, 'x': 0.5},
-        #           title={'text': f'Excess deaths in {selected_dropdown_value} in 2020','y':0.9,'x':0.5,'xanchor': 'center',
-        # 'yanchor': 'top'},
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-                  template='plotly_dark',
-                  xaxis_title='Week Number',
-                  yaxis_title='Number of Deaths',
-                  font=dict(
-                        family="Roboto",
-                        size=18),
-                  paper_bgcolor='rgba(0, 0, 0, 0)',
-                  plot_bgcolor='rgba(0, 0, 0, 0)',
-                  margin={'b': 15},
-                  hovermode='x',
-                  autosize=True,
-                  xaxis={'range': [df_sub['Week_Number'].min(), df_sub['Week_Number'].max()]}, 
-                  yaxis={'range': [0, (df_sub['reg_deaths_20'].max())]}
-              )
-    figure=go.Figure(data=data, layout=layout)
-
-
-    return figure
-
-#callback graph title
-@app.callback(Output("graph-title", "children"), [Input("ladselector", "value")])
-def update_graph_title(selected_dropdown_value):
-    return f"Graph of excess deaths in 2020 in  {selected_dropdown_value}"
-
-
-#callback total excess deaths text
-@app.callback(Output("total-num", "children"), [Input("ladselector", "value")])
-def update_total_num(selected_dropdown_value):
-    return f"The total number of excess deaths in 2020 in {selected_dropdown_value} is {round(np.sum(df[df['Area_Name'] == selected_dropdown_value]['excess_deaths_per_100t_20']), 1)} per 100,000 inhabitants."
-    
+# Callback for map deaths
 @app.callback(Output('map', 'figure'),
               [Input('period', 'value'),
               Input('weeks-slider', 'value')], 
@@ -280,21 +186,9 @@ def update_chloro(period, selected_value, figure):
             Input('weeks-slider', 'value')])
 def update_map_title(period, selected_value):
     if period == 'Yearly':
-        return f'Map of yearly excess deaths in 2020.'
+        return f'Yearly excess deaths in 2020.'
     if period == 'Weekly':
-        return f'Map of excess deaths in week {selected_value} of 2020'
-
-#call back weekly excess deaths:
-
-@app.callback(Output('weekly-excess-deaths', 'children'),
-            [Input('period', 'value'),
-            Input('weeks-slider', 'value')])
-def update_w_ex_deaths(period, selected_value):
-    if period == 'Yearly':
-        return f"The total excess deaths in 2020 compared to the average of 2015-2019 was {round(np.sum(df[df['Area_Name'].isin(['England', 'Wales'])]['excess_deaths_per_100t_20']), 1)} per 100,000 inhabitants"
-    if period == 'Weekly':
-        return f"The total excess deaths in week {selected_value} compared to the average of 2015-2019 was {round(np.sum(df_maps[df_maps['Week_Number'] == selected_value]['excess_deaths_per_100t_20']), 1)} per 100,000 inhabitants"
-
+        return f'Excess deaths in week {selected_value}'
 
 
 if __name__ == '__main__':
